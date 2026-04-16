d3514ab77c50b053854272ec004a93f35079eb7a
alfa0302 <alfiyanijin@gmail.com>
2026-04-16 15:22:56 +0400

    Installed required libraries, defined app routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,36 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import Login from "./pages/auth/Login";
+import SignUp from "./pages/auth/SignUp";
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/home/Dashboard";
+import InterviewPrep from "./pages/interview-prep/InterviewPrep";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} /> */}
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/interview-prep/:sessionId"
+            element={<InterviewPrep />}
+          />
+        </Routes>
+      </BrowserRouter>
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontsize: "13px",
+          },
+        }}
+      />
+    </div>
+  );
 }
